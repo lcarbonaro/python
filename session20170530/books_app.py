6f5cd6ef6e197c51bbaf86576bf1949dd45f6dbd
Les Carbonaro <lcarbonaro@gmail.com>
2017-06-04 23:02:12 +0000

    corrected inconsistent use of quotes

--- session20170530/books_app.py
+++ session20170530/books_app.py
@@ -4,31 +4,31 @@ import sqlite3
 import os
 
 # for c9 users
-host = os.getenv("IP", "0.0.0.0")
-port = int(os.getenv("PORT", "8080"))
+host = os.getenv('IP', '0.0.0.0')
+port = int(os.getenv('PORT', '8080'))
 
 # configuration details
-DATABASE = "books.db"
-SECRET_KEY = "my_secret"
+DATABASE = 'books.db'
+SECRET_KEY = 'my_secret'
 
 app = Flask(__name__)
 app.config.from_object(__name__) # pulls in app configuration by looking for uppercase variables
 
 # create function to connect to database
 def connect_db():
-    return sqlite3.connect(app.config["DATABASE"])
+    return sqlite3.connect(app.config['DATABASE'])
     
     
 # create routes using @app.route decorator
 
 # log in route
-@app.route("/", methods=["GET","POST"])
+@app.route('/', methods=['GET','POST'])
 def login():
     message = ''
     if request.method == 'POST':
         session.pop('username',None)   # clear session variable
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form['username']
+        password = request.form['password']
         
         # temporarily set as vars
         # later read from database
@@ -41,10 +41,10 @@ def login():
         else:
             message = 'Invalid log-in credentials!'
             
-    return render_template("login.html", msg=message)
+    return render_template('login.html', msg=message)
     
 # log out route
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     session.pop('username',None)   # clear session variable
     return redirect( url_for('login') )
@@ -59,7 +59,7 @@ def before_request():
         return redirect( url_for('login') )
 
 # main route (list books)
-@app.route("/main")
+@app.route('/main')
 def main():
            
     # create connection to db
@@ -67,7 +67,7 @@ def main():
     # create a cursor to db
     cursor = g.conn.cursor()
     # execute a query against db
-    cursor.execute("SELECT * FROM books")
+    cursor.execute('SELECT * FROM books')
     books_data = cursor.fetchall()
     
     # how do we want to display the data? In a list
@@ -82,42 +82,42 @@ def main():
     g.conn.close()
     
     # pass books data to our template for use
-    return render_template("main.html", books=books)
+    return render_template('main.html', books=books)
     
     
 # add route to add book
-@app.route("/add", methods=["GET","POST"])
+@app.route('/add', methods=['GET','POST'])
 def add_book():
     
-    msg = ""
-    if request.method == "POST":
-        author = request.form["author"]
-        title = request.form["title"]
-        genre = request.form["genre"]
-        price = request.form["price"]
+    msg = ''
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        genre = request.form['genre']
+        price = request.form['price']
         
         g.conn = connect_db()
         cursor = g.conn.cursor()
-        cursor.execute("INSERT INTO books (author, title, genre, price) VALUES(?, ?, ?, ?)", (author, title, genre, price))
+        cursor.execute('INSERT INTO books (author, title, genre, price) VALUES(?, ?, ?, ?)', (author, title, genre, price))
         
         g.conn.commit()
         g.conn.close()
-        msg = "Record successfully added!"
+        msg = 'Record successfully added!'
     
     return render_template('add.html', message=msg)
     
 # edit route
-@app.route("/edit", methods=["GET", "POST"])
+@app.route('/edit', methods=['GET', 'POST'])
 def edit():
     
-    msg = ""
+    msg = ''
     
     # get query string arguments
-    book_id = request.args.get("book_id")
-    author = request.args.get("author")
-    title = request.args.get("title")
-    genre = request.args.get("genre")
-    price = request.args.get("price")
+    book_id = request.args.get('book_id')
+    author = request.args.get('author')
+    title = request.args.get('title')
+    genre = request.args.get('genre')
+    price = request.args.get('price')
     
     book = {
         "book_id": book_id,
@@ -127,18 +127,18 @@ def edit():
         "price":price
     }
     
-    if request.method == "POST":
+    if request.method == 'POST':
         # get the data from form
-        book_id = request.form["book_id"]
-        author = request.form["author"]
-        title = request.form["title"]
-        genre = request.form["genre"]
-        price = request.form["price"]
+        book_id = request.form['book_id']
+        author = request.form['author']
+        title = request.form['title']
+        genre = request.form['genre']
+        price = request.form['price']
         
         # connect db and update record
         g.conn = connect_db()
         cursor = g.conn.cursor()
-        cursor.execute("UPDATE books SET author=?, title=?, genre=?, price=? WHERE id=?", (author, title, genre, price,book_id))
+        cursor.execute('UPDATE books SET author=?, title=?, genre=?, price=? WHERE id=?', (author, title, genre, price,book_id))
         
         g.conn.commit()
         g.conn.close()
@@ -151,20 +151,20 @@ def edit():
             "price":price
         }
         
-        msg = "Record successfully updated!"
+        msg = 'Record successfully updated!'
     
     return render_template('edit.html', book_record=book, message=msg)
         
 # delete route
-@app.route("/delete", methods=["GET", "POST"])
+@app.route('/delete', methods=['GET', 'POST'])
 def delete():
     
     # get query string arguments
-    book_id = request.args.get("book_id")
-    author = request.args.get("author")
-    title = request.args.get("title")
-    genre = request.args.get("genre")
-    price = request.args.get("price")
+    book_id = request.args.get('book_id')
+    author = request.args.get('author')
+    title = request.args.get('title')
+    genre = request.args.get('genre')
+    price = request.args.get('price')
     
     book = {
         "book_id": book_id,
@@ -174,15 +174,15 @@ def delete():
         "price":price
     }
     
-    if request.method == "POST":
+    if request.method == 'POST':
         
         # get the data from form
-        book_id = request.form["book_id"]
+        book_id = request.form['book_id']
         
         # connect db and delete record
         g.conn = connect_db()
         cursor = g.conn.cursor()
-        cursor.execute("DELETE FROM books WHERE id=?", (book_id))
+        cursor.execute('DELETE FROM books WHERE id=?', (book_id))
         
         g.conn.commit()
         g.conn.close()
@@ -194,7 +194,7 @@ def delete():
 
 
 # start the Flask application    
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host=host, port=port, debug=True)
     
     
